0481b16367f4f6733ad81786dc79ff71133d6bc4
Senal Kumarage <senalkumarage@gmail.com>
2019-04-14 21:18:15 +0800

    Connecting to the server

---
+++ task-manager/mongodb.js
@@ -0,0 +1,18 @@
+
+const mongodb = require('mongodb')
+
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser : true }, (error, client) => {
+
+    if(error) {
+
+        return console.log('Unable connect to the database');
+    } else {
+
+        console.log('Connected correctly!');
+    }
+})
