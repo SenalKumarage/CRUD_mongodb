0a129ce5212b7f05f7646ed519ffb20ee6dea849
Senal Kumarage <senalkumarage@gmail.com>
2019-04-28 14:21:41 +0800

    Inserting data
    Using insertOne and insertMany

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -6,13 +6,73 @@ const MongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, { useNewUrlParser : true }, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
 
-    if(error) {
+    if (error) {
 
         return console.log('Unable connect to the database');
-    } else {
-
-        console.log('Connected correctly!');
     }
+
+    const db = client.db(databaseName)
+
+    // db.collection('users').insertOne({
+    //     name: 'Senal',
+    //     age: 28
+    // }, (error, result) => {
+
+    //     if(error) {
+
+    //         return console.log('Unable to insert document');
+    //     }
+
+    //     console.log(result.ops);
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28,
+    //         gender: 'f'
+
+    //     },
+    //     {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+
+    //     if (error) {
+
+    //         return console.log('Unable to insert records');
+    //     }
+
+    //     console.log(result.ops);
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Task 1',
+            completed: true
+        },
+        {
+            description: 'Task 2',
+            completed: false
+        },
+        {
+            description: 'Task 3',
+            completed: true
+        },
+        {
+            description: 'Task 4',
+            completed: false
+        }
+    ], (error, result) => {
+
+        if(error) {
+
+            return console.log('Unable to insert tasks')
+        }
+
+        console.log(result.ops);
+    })
 })
